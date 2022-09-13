0724c02cd1a859f0f72f215b8063b7b8466e6673
玊非玉 <2020991235@qq.com>
2022-09-13 14:46:03 +0800

    玊非玉：完成两个句子之间对比的contrast方法，完成句子之间连续对比的continuous_contrast方法，完成计算重复率的calculate方法，基本完成简单的查重功能，下一步进行验证。

--- src/package_01/Main.java
+++ src/package_01/Main.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
+    //划分句子
     public void sentence_division(File file,List<Sentence> list_sentence){
         int c;
         List<Character> list_char=new LinkedList<>();
@@ -12,7 +13,8 @@ public class Main {
             BufferedReader bufferedReader=new BufferedReader(new FileReader(file));
             while((c= bufferedReader.read())!=-1){
                 if(Character.toString(c).matches("[.。！!?？…]")&&!list_char.isEmpty()){
-                    list_sentence.add(new Sentence(list_char,list_char.size(),0));
+                    List<Character> list_clone=new LinkedList<>(list_char);
+                    list_sentence.add(new Sentence(list_clone,list_char.size(),0));
                     list_char.clear();
                 }
                 else if(!Character.toString(c).matches("[,，、：:\"“”\s\n\r《》*()（）—]"))
@@ -24,46 +26,76 @@ public class Main {
         }
     }
 
-    public void contrast(Sentence sentence_origin,Sentence sentence_plagiarism){
-        /*int size_origin=list_origin.size();
-        int size_plagiarism=list_plagiarism.size();
-        for(int i=0;i<size_origin;i++) {
-            for (int j = 0; j < size_plagiarism; j++) {
-                if(list_origin.get(i).equals(list_plagiarism.get(j))){
-                    list_origin.remove(i);
-                    list_plagiarism.remove(j);
-                    size_origin-=1;
-                    size_plagiarism-=1;
-                    i=-1;
-                    break;
+    //句子之间连续对比
+    public void continuous_contrast(List<Sentence> list_sentence_origin,List<Sentence> list_sentence_plagiarism) {
+        int repeat_number, size;
+        float repeat_rate;
+        for (int i = 0; i < list_sentence_plagiarism.size(); i++) {
+            size = list_sentence_plagiarism.get(i).getSize();
+            for (int j = i; j < list_sentence_origin.size(); j++) {
+                repeat_number = contrast(list_sentence_origin.get(j), list_sentence_plagiarism.get(i));
+                repeat_rate = (float) repeat_number / size;
+                if (size < 10) {
+                    if (repeat_rate > 0.6) {
+                        list_sentence_plagiarism.get(i).setRepeat_number(repeat_number);
+                        break;
+                    }
+                } else {
+                    if (repeat_rate > 0.5) {
+                        list_sentence_plagiarism.get(i).setRepeat_number(repeat_number);
+                        break;
+                    }
                 }
             }
-        }*/
+            if(list_sentence_plagiarism.get(i).getRepeat_number()==0) {
+                for (int j = 0; j < i; j++) {
+                    repeat_number = contrast(list_sentence_origin.get(j), list_sentence_plagiarism.get(i));
+                    repeat_rate = (float) repeat_number / size;
+                    if (size < 10) {
+                        if (repeat_rate > 0.6) {
+                            list_sentence_plagiarism.get(i).setRepeat_number(repeat_number);
+                            break;
+                        }
+                    } else {
+                        if (repeat_rate > 0.5) {
+                            list_sentence_plagiarism.get(i).setRepeat_number(repeat_number);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    public static void main(String[] args) {
-        List<Character> list_origin=new LinkedList<>();
-        List<Character> list_plagiarism=new LinkedList<>();
-        File file_origin=new File("./test_text/orig.txt");
-        File file_plagiarism=new File("./test_text/orig_0.8_dis_15.txt");
-        char c;
-        try {
-            BufferedReader bufferedReader_origin=new BufferedReader(new FileReader(file_origin));
-            while((c= (char) bufferedReader_origin.read())!='。'){
-                if(!Character.toString(c).matches("[,，、：:\"“\s\n\r]"))list_origin.add(c);
-            }
-            bufferedReader_origin.close();
+    //两个句子之间对比
+    public int contrast(Sentence sentence_origin,Sentence sentence_plagiarism){
+        List<Character> list_origin=sentence_origin.getList();
+        List<Character> list_plagiarism=sentence_plagiarism.getList();
+        int plagiarism_size=sentence_plagiarism.getSize();
+        list_plagiarism.removeAll(list_origin);
+        return plagiarism_size-list_plagiarism.size();
+    }
 
-            BufferedReader bufferedReader_plagiarism=new BufferedReader(new FileReader(file_plagiarism));
-            while((c= (char) bufferedReader_plagiarism.read())!='。'){
-                if(!Character.toString(c).matches("[,，、：:\"“\s\n\r]"))list_plagiarism.add(c);
-            }
-            bufferedReader_plagiarism.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    //计算重复率
+    public float calculate(List<Sentence> list_sentence_plagiarism){
+        int size = 0,repeat_number=0;
+        for (Sentence sentence : list_sentence_plagiarism) {
+            size += sentence.getSize();
+            repeat_number += sentence.getRepeat_number();
         }
-        System.out.println(list_origin);
-        System.out.println();
-        System.out.println(list_plagiarism);
+        return (float)Math.round((float)repeat_number/size*100)/100;
+    }
+
+
+    public static void main(String[] args) {
+        List<Sentence> list_sentence_origin = new LinkedList<>();
+        List<Sentence> list_sentence_plagiarism = new LinkedList<>();
+        File file_origin = new File("./test_text/orig.txt");
+        File file_plagiarism = new File("./test_text/orig_0.8_del.txt");
+        Main main=new Main();
+        main.sentence_division(file_origin,list_sentence_origin);
+        main.sentence_division(file_plagiarism,list_sentence_plagiarism);
+        main.continuous_contrast(list_sentence_origin,list_sentence_plagiarism);
+        System.out.println("重复率为："+main.calculate(list_sentence_plagiarism));
     }
 }
