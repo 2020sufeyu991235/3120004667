975fa1aa42e02328df0c069623266f51fbea732f
玊非玉 <2020991235@qq.com>
2022-09-16 18:09:17 +0800

    玊非玉：sentence_division句子划分对文件读取的解码格式设置为UTF-8，contrast句子对比将传入的Sentence_plagiarism克隆生成list_plagiarism

--- src/package_01/Main.java
+++ src/package_01/Main.java
@@ -1,6 +1,7 @@
 package package_01;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,7 +11,8 @@ public class Main {
         int c;
         List<Character> list_char=new LinkedList<>();
         try {
-            BufferedReader bufferedReader=new BufferedReader(new FileReader(file));
+            //BufferedReader bufferedReader=new BufferedReader(new FileReader(file));
+            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
             while((c= bufferedReader.read())!=-1){
                 if(Character.toString(c).matches("[.。！!?？…]")&&!list_char.isEmpty()){
                     List<Character> list_clone=new LinkedList<>(list_char);
@@ -30,18 +32,20 @@ public class Main {
     public void continuous_contrast(List<Sentence> list_sentence_origin,List<Sentence> list_sentence_plagiarism) {
         int repeat_number, size;
         float repeat_rate;
-        for (int i = 0; i < list_sentence_plagiarism.size(); i++) {
+        int i;
+        System.out.println(list_sentence_plagiarism.size());
+        for (i = 0; i < list_sentence_plagiarism.size(); i++) {
             size = list_sentence_plagiarism.get(i).getSize();
             for (int j = i; j < list_sentence_origin.size(); j++) {
                 repeat_number = contrast(list_sentence_origin.get(j), list_sentence_plagiarism.get(i));
                 repeat_rate = (float) repeat_number / size;
                 if (size < 10) {
-                    if (repeat_rate > 0.6) {
+                    if (repeat_rate > 0.75) {
                         list_sentence_plagiarism.get(i).setRepeat_number(repeat_number);
                         break;
                     }
                 } else {
-                    if (repeat_rate > 0.5) {
+                    if (repeat_rate > 0.7) {
                         list_sentence_plagiarism.get(i).setRepeat_number(repeat_number);
                         break;
                     }
@@ -52,25 +56,27 @@ public class Main {
                     repeat_number = contrast(list_sentence_origin.get(j), list_sentence_plagiarism.get(i));
                     repeat_rate = (float) repeat_number / size;
                     if (size < 10) {
-                        if (repeat_rate > 0.6) {
+                        if (repeat_rate > 0.75) {
                             list_sentence_plagiarism.get(i).setRepeat_number(repeat_number);
                             break;
                         }
                     } else {
-                        if (repeat_rate > 0.5) {
+                        if (repeat_rate > 0.7) {
                             list_sentence_plagiarism.get(i).setRepeat_number(repeat_number);
                             break;
                         }
                     }
                 }
+                //System.out.println(i);
             }
         }
+        //System.out.println(i);
     }
 
     //两个句子之间对比
     public int contrast(Sentence sentence_origin,Sentence sentence_plagiarism){
         List<Character> list_origin=sentence_origin.getList();
-        List<Character> list_plagiarism=sentence_plagiarism.getList();
+        List<Character> list_plagiarism=new LinkedList<>(sentence_plagiarism.getList());
         int plagiarism_size=sentence_plagiarism.getSize();
         list_plagiarism.removeAll(list_origin);
         return plagiarism_size-list_plagiarism.size();
@@ -91,7 +97,9 @@ public class Main {
         List<Sentence> list_sentence_origin = new LinkedList<>();
         List<Sentence> list_sentence_plagiarism = new LinkedList<>();
         File file_origin = new File("./test_text/orig.txt");
-        File file_plagiarism = new File("./test_text/orig_0.8_del.txt");
+        File file_plagiarism = new File("./test_text/orig_0.8_dis_15.txt");
+        /*File file_origin = new File(args[0]);
+        File file_plagiarism = new File(args[1]);*/
         Main main=new Main();
         main.sentence_division(file_origin,list_sentence_origin);
         main.sentence_division(file_plagiarism,list_sentence_plagiarism);
